3b30db2fcedfc3ec2116b33d52958482559d8caf
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-29 14:50:10 +0100

    Added click event listener to chevron

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 "use strict";
 
 window.onload = function(event) {
-	document.getElementById("copyright-year").textContent = (new Date()).getFullYear()
+	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	function toggle(chevron) {
+		
+	}
+
+	let chevron = document.getElementsByClassName("fas")[0];
+	chevron.addEventListener("click", function(event) {
+		toggle(this);
+	});
 };
