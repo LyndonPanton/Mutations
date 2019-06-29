2c7f1002af4c11c4de61aef2ac4e22ad328565c0
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-29 15:32:31 +0100

    Prevent non alphabetical characters

--- js/app.js
+++ js/app.js
@@ -4,10 +4,14 @@ window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function check(first, second) {
+		let regex = /[^a-zA-Z]/gi;
+
 		if (!first || !second) {
 			return display("???");
 		} else if (first.indexOf(" ") !== -1 || second.indexOf(" ") !== -1) {
 			return display("Input fields must not contain white space");
+		} else if (regex.test(first) || regex.test(second)) {
+			return display("Input must only contain alphabetical characters");
 		} else {
 			first = first.toLowerCase();
 			second = second.toLowerCase();
